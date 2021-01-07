cb2157199cc6d04f393b0968766709ab0138c737
ByromJomaa <55561544+ByromJomaa@users.noreply.github.com>
2021-01-07 03:03:08 -0500

    Replace AppBar

--- family_shopping_list/lib/screens/home/home.dart
+++ family_shopping_list/lib/screens/home/home.dart
@@ -30,38 +30,78 @@ class Home extends StatelessWidget {
       value: DatabaseService().shoppingList,
       child: Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text(
-            'Family Shopping List',
-            style: TextStyle(
-              color: Colors.black,
+        // appBar: AppBar(
+        //   title: Text(
+        //     'Family Shopping List',
+        //     style: TextStyle(
+        //       color: Colors.black,
+        //     ),
+        //   ),
+        //   backgroundColor: Colors.blue[300],
+        //   elevation: 0.0,
+        //   actions: <Widget>[
+        //     FlatButton.icon(
+        //       icon: Icon(Icons.person),
+        //       label: Text('Log out'),
+        //       onPressed: () async {
+        //         await _auth.signOut();
+        //       },
+        //     ),
+        //     FlatButton.icon(
+        //       icon: Icon(Icons.settings),
+        //       label: Text('Settings'),
+        //       onPressed: () => _showSettingsPanel(),
+        //     ),
+        //   ],
+        // ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/coffee_bg.png'),
+              fit: BoxFit.cover,
             ),
           ),
-          backgroundColor: Colors.blue[300],
-          elevation: 0.0,
-          actions: <Widget>[
-            FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('Log out'),
-              onPressed: () async {
-                await _auth.signOut();
-              },
-            ),
-            FlatButton.icon(
-              icon: Icon(Icons.settings),
-              label: Text('Settings'),
-              onPressed: () => _showSettingsPanel(),
-            ),
-          ],
-        ),
-        body: Container(
-          // decoration: BoxDecoration(
-          //   image: DecorationImage(
-          //     image: AssetImage('assets/coffee_bg.png'),
-          //     fit: BoxFit.cover,
-          //   ),
-          // ),
-          child: ShoppingListList(),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 30),
+              SizedBox(
+                height: 135,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 25.0),
+                    Text(
+                      '  Family Shopping List',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        FlatButton.icon(
+                          icon: Icon(Icons.person),
+                          label: Text('Log out'),
+                          onPressed: () async {
+                            await _auth.signOut();
+                          },
+                        ),
+                        FlatButton.icon(
+                          icon: Icon(Icons.settings),
+                          label: Text('Settings'),
+                          onPressed: () => _showSettingsPanel(),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                child: ShoppingListList(),
+              ),
+            ],
+          ),
         ),
       ),
     );
